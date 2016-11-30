050364a3431ef4157e7fbd75d4a2f62d948a5561
wordi <wordijp@gmail.com>
2016-12-01 00:01:56 +0900

    implement

--- lib/git/stash/commit/version.rb
+++ lib/git/stash/commit/version.rb
@@ -1,7 +1,7 @@
 module Git
   module Stash
     module Commit
-      VERSION = "0.1.0"
+      VERSION = "1.0.0"
     end
   end
 end
---
+++ lib/git/stash/sclib/define.rb
@@ -0,0 +1,7 @@
+# 定数
+
+PREFIX = 'stash-commit'
+TMP_SUFFIX = 'progresstmp'
+PATCH_REMAIN_SUFFIX = 'patch-remain'
+BACKUP_SUFFIX = 'backup'
+
