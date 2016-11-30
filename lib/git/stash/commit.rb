472d9dce7f6440d901a654db630ef946dc5c41d4
wordi <wordijp@gmail.com>
2016-11-30 19:57:13 +0900

    Initial commit

---
+++ lib/git/stash/commit.rb
@@ -0,0 +1,9 @@
+require "git/stash/commit/version"
+
+module Git
+  module Stash
+    module Commit
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/git/stash/commit/version.rb
@@ -0,0 +1,7 @@
+module Git
+  module Stash
+    module Commit
+      VERSION = "0.1.0"
+    end
+  end
+end
